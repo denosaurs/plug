3d60682a56fba2ab9bd1d8f0b6d3ff58c166e4bc
eliassjogreen <eliassjogreen1@gmail.com>
2020-09-13 21:55:06 +0200

    upgrade: deno 1.4.0 support

--- deps.ts
+++ deps.ts
@@ -2,13 +2,13 @@ export {
   resolve,
   join,
   extname,
-} from "https://deno.land/std@0.61.0/path/mod.ts";
+} from "https://deno.land/std@0.69.0/path/mod.ts";
 
 export {
   exists,
   ensureDir,
-} from "https://deno.land/std@0.61.0/fs/mod.ts";
+} from "https://deno.land/std@0.69.0/fs/mod.ts";
 
-export { createHash } from "https://deno.land/std@0.61.0/hash/mod.ts";
+export { createHash } from "https://deno.land/std@0.69.0/hash/mod.ts";
 
-export { Cache } from "https://deno.land/x/cache@0.0.6/mod.ts";
+export { Cache } from "https://deno.land/x/cache@0.2.0/mod.ts";
--- test_deps.ts
+++ test_deps.ts
@@ -1,18 +1,18 @@
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.61.0/testing/asserts.ts";
+} from "https://deno.land/std@0.69.0/testing/asserts.ts";
 
-export { serve } from "https://deno.land/std@0.61.0/http/server.ts";
-export { serveFile } from "https://deno.land/std@0.61.0/http/file_server.ts";
+export { serve } from "https://deno.land/std@0.69.0/http/server.ts";
+export { serveFile } from "https://deno.land/std@0.69.0/http/file_server.ts";
 
 export {
   join,
   resolve,
   dirname,
   fromFileUrl,
-} from "https://deno.land/std@0.61.0/path/mod.ts";
+} from "https://deno.land/std@0.69.0/path/mod.ts";
 
-export { exists } from "https://deno.land/std@0.61.0/fs/mod.ts";
+export { exists } from "https://deno.land/std@0.69.0/fs/mod.ts";
 
-export { Cache } from "https://deno.land/x/cache@0.0.5/mod.ts";
+export { Cache } from "https://deno.land/x/cache@0.2.0/mod.ts";
