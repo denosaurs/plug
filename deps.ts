227292a50beb80481fc48d68f8706081c53feab2
eliassjogreen <eliassjogreen1@gmail.com>
2020-09-19 22:19:12 +0200

    upgrade: std@0.70.0

--- deps.ts
+++ deps.ts
@@ -2,13 +2,13 @@ export {
   resolve,
   join,
   extname,
-} from "https://deno.land/std@0.69.0/path/mod.ts";
+} from "https://deno.land/std@0.70.0/path/mod.ts";
 
 export {
   exists,
   ensureDir,
-} from "https://deno.land/std@0.69.0/fs/mod.ts";
+} from "https://deno.land/std@0.70.0/fs/mod.ts";
 
-export { createHash } from "https://deno.land/std@0.69.0/hash/mod.ts";
+export { createHash } from "https://deno.land/std@0.70.0/hash/mod.ts";
 
-export { Cache } from "https://deno.land/x/cache@0.2.0/mod.ts";
+export { Cache } from "https://deno.land/x/cache@0.2.1/mod.ts";
