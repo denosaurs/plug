8746880c200026bade6499a9fc16a50a5c97cac5
eliassjogreen <eliassjogreen1@gmail.com>
2023-01-18 19:08:28 +0100

    chore: remove permission checks, bump dependencies

--- deps.ts
+++ deps.ts
@@ -7,7 +7,7 @@ export {
   normalize,
   resolve,
   toFileUrl,
-} from "https://deno.land/std@0.157.0/path/mod.ts";
-export { ensureDir } from "https://deno.land/std@0.157.0/fs/mod.ts";
-export { encode as hex } from "https://deno.land/std@0.157.0/encoding/hex.ts";
-export * as colors from "https://deno.land/std@0.157.0/fmt/colors.ts";
+} from "https://deno.land/std@0.173.0/path/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.173.0/fs/mod.ts";
+export { encode as hex } from "https://deno.land/std@0.173.0/encoding/hex.ts";
+export * as colors from "https://deno.land/std@0.173.0/fmt/colors.ts";
