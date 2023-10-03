84a4b707caf5928bee0aa63cea5f051960467142
Elias Sjögreen <elias@5monkeys.se>
2023-10-03 12:35:57 +0200

    chore: bump dependencies

--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.188.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.188.0/path/mod.ts";
+export * from "https://deno.land/std@0.203.0/assert/mod.ts";
+export * from "https://deno.land/std@0.203.0/path/mod.ts";
