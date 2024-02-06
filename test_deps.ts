826536a4350d46fd2c191d6bc90b39912a631bec
Divy Srivastava <dj.srivastava23@gmail.com>
2024-02-06 14:57:37 +0530

    Update deps

--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.203.0/assert/mod.ts";
-export * from "https://deno.land/std@0.203.0/path/mod.ts";
+export * from "https://deno.land/std@0.214.0/assert/mod.ts";
+export * from "https://deno.land/std@0.214.0/path/mod.ts";
